abad8352484a42e8a39e3f8f2ea0e75b86a4c902
Jason Ni <jasonni@ucsb.edu>
2023-07-29 14:47:58 -0400

    multithreaded api calls

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import openai
+import threading
 from flask import Flask, render_template, request, redirect, url_for, session
-import re
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,51 +10,71 @@ app = Flask(__name__)
 # app.config.from_object(Config)
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
+def getSymptoms(diagnosis, res):
+    sympResponse = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"lowercase comma delimited list of the symptoms of {diagnosis}",
+        temperature=0.7,
+        max_tokens=714,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    res[0] = sympResponse
+
+def getMedTreatments(diagnosis, res):
+    treatmedResponse = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"Go in depth in a paragraph on the medical treatments(specific names and brands) of {diagnosis} in sentences, then go in depth in a separate paragraph on the home treatments of {diagnosis} in sentences.",
+        temperature=0.7,
+        max_tokens=714,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    res[0] = treatmedResponse
+
+def getPlan(diagnosis, res):
+    planResponse = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"How to prevent {diagnosis} (lowercase comma delimited list) and how long(specific time) after doctor visit should patient return to doctor office?",
+        temperature=0.7,
+        max_tokens=714,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    res[0] = planResponse
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if 'diagnosis' in request.form:
         diagnosis = request.form["diagnosis"]
-        sympResponse = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=f"lowercase comma delimited list of the symptoms of {diagnosis}",
-            temperature=0.7,
-            max_tokens=714,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-        symptom = sympResponse["choices"][0]["text"]
-        treatmedResponse = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=f"Go in depth in a paragraph on the medical treatments(specific names and brands) of {diagnosis} in sentences.",
-            temperature=0.7,
-            max_tokens=714,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-        treathomeResponse = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=f"Go in depth in a paragraph on the home treatments of {diagnosis} in sentences.",
-            temperature=0.7,
-            max_tokens=714,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-        medTreat = treatmedResponse["choices"][0]["text"]
-        homeTreat = treathomeResponse["choices"][0]["text"]
-        planResponse = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=f"How to prevent {diagnosis} (lowercase comma delimited list) and how long(specific time) after doctor visit should patient return to doctor office?",
-            temperature=0.7,
-            max_tokens=714,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-        plan = planResponse["choices"][0]["text"]
-        return render_template('home.html', diagnosis=diagnosis, symptoms=symptom, medTreat=medTreat, homeTreat=homeTreat, plan=plan)
+
+        # create return variables for symptoms, treatments, and plan
+        sympResponse = [None]
+        treatmedResponse = [None]
+        planResponse = [None]
+
+        # create threads for symptoms, treatments, and plan
+        sympThread = threading.Thread(target=getSymptoms, args=(diagnosis, sympResponse))
+        treatThread = threading.Thread(target=getMedTreatments, args=(diagnosis, treatmedResponse))
+        planThread = threading.Thread(target=getPlan, args=(diagnosis, planResponse))
+
+        # start threads and wait for all completion
+        sympThread.start()
+        treatThread.start()
+        planThread.start()
+        sympThread.join()
+        treatThread.join()
+        planThread.join()
+        
+        symptom = sympResponse[0]["choices"][0]["text"]
+        medTreat = treatmedResponse[0]["choices"][0]["text"]
+        plan = planResponse[0]["choices"][0]["text"]
+
+        return render_template('home.html', diagnosis=diagnosis, symptoms=symptom, medTreat=medTreat, plan=plan)
     return render_template('home.html')
 
 if __name__ == "__main__":
